b704aa6a76fab255c149b9f24021df9706e99166
adityaagrawal <aditya33agrawal@gmail.com>
2021-07-18 16:12:26 +0530

    changes in hangman

---
+++ Hangman Game/js/app.js
@@ -0,0 +1,35 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['application', 'programming', 'hardware', 'wizard', 'hangman', 'interface'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetter = ['a'];
+const wrongLetters = [];
+
+//show the hidden word
+function displayWord() {
+  wordEl.innerHTML = `
+    ${selectedWord
+      .split('')
+      .map(
+        letter => `
+          <span class="letter">
+            ${correctLetters.includes(letter) ? letter : ''}
+          </span>
+        `
+      )
+      .join('')}
+  `;
+
+   
+}
+
+displayWord();
